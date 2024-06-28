3e180c97f6a0fc03375befde7d77981e52ececf4
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-06-28 22:11:30 +0530

    Time: 1 ms (99.76%), Space: 44.5 MB (79.85%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -44,18 +44,5 @@ class Solution {
         }
         return res;
     }
-    public static void main(String args[]) {
-        int N = 4;
-        List < List < String >> queen = solveNQueens(N);
-        int i = 1;
-        for (List < String > it: queen) {
-            System.out.println("Arrangement " + i);
-            for (String s: it) {
-                System.out.println(s);
-            }
-            System.out.println();
-            i += 1;
-        }
-
-    }
+    
 }
