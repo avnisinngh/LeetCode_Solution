d95b0ad49953ac8309ae0540f8cdbf2de8646873
avnisinngh <150375257+avnisinngh@users.noreply.github.com>
2024-10-29 21:32:15 +0530

    Time: 6 ms (80.77%), Space: 56.4 MB (42.75%) - LeetHub

---
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    private final int[] dirs = { -1, 0, 1 };
+
+    private int DFS(int row, int col, int[][] grid, int[][] dp) {
+        int M = grid.length, N = grid[0].length;
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+
+        int maxMoves = 0;
+        for (int dir : dirs) {
+            int newRow = row + dir, newCol = col + 1;
+            if (
+                newRow >= 0 &&
+                newCol >= 0 &&
+                newRow < M &&
+                newCol < N &&
+                grid[row][col] < grid[newRow][newCol]
+            ) {
+                maxMoves = Math.max(
+                    maxMoves,
+                    1 + DFS(newRow, newCol, grid, dp)
+                );
+            }
+        }
+
+        dp[row][col] = maxMoves;
+        return maxMoves;
+    }
+
+    public int maxMoves(int[][] grid) {
+        int M = grid.length, N = grid[0].length;
+
+        int[][] dp = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int maxMoves = 0;
+        for (int i = 0; i < M; i++) {
+            int movesRequired = DFS(i, 0, grid, dp);
+            maxMoves = Math.max(maxMoves, movesRequired);
+        }
+
+        return maxMoves;
+    }
+}
